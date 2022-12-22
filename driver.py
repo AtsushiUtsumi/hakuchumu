706e57001bc142dc10cae939303067f72b4cbb91
AtsushiUtsumi <debtgx103@gmail.com>
2022-12-22 16:03:34 +0900

    リファクタリング

--- driver.py
+++ driver.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome import service as fs
 from selenium.webdriver.chrome.options import Options
 
