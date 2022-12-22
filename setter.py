4f57549cc1034c86484ee3ed68d03f256bd7291c
AtsushiUtsumi <debtgx103@gmail.com>
2022-12-22 12:48:40 +0900

    実行コードを出力するコードを作成

--- setter.py
+++ setter.py
@@ -16,3 +16,22 @@ def create_setter(id, driver):
 # つまりcreate_setter関数はページ毎に手動で書かないといけない。
 # じゃ、どこを自動で書くかといううと
 # 出力はDBに丸投げるので
+
+
+
+
+# 出力したいコードをイメージ出来ているのか
+def append_setter(file, test_case):
+    file.write("print('これはテストコードです')")
+    return
+
+def create_test_code_file(input_sequence, check_output):
+    file = open('auto.py', 'w', encoding='utf8')
+    file.write("# -*- coding: utf-8 -*-\n")
+    append_setter(file, None)
+    file.close()
+    return
+
+create_test_code_file('', '')
+
+import auto#これで実行はあまりにダサいがひとまずこれで...
