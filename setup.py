97ef4727a801be90cf3ac1f6464e6acb5f727fd3
AtsushiUtsumi <debtgx103@gmail.com>
2023-01-13 22:53:35 +0900

    テスト対象のディレクトリ内に設定用フォルダを用意する

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 # コマンドライン引数で「テスト対象の作業ディレクトリ」を渡す
 import sys
-target_workdir = sys.argv[1]
+target_workdir = './'
+if len(sys.argv) != 2:
+    print('作業ディレクトリが入力されていません')
+else:
+    target_workdir = sys.argv[1]
 
 import os
 #「python setup.py /ram」でTrue
-if os.path.isdir(target_workdir):
-    os.chdir(target_workdir)
-else:
+if not os.path.isdir(target_workdir):
     print(f'指定されたパスが見つかりません: {target_workdir}')
     exit(1)
 
-if not os.path.isdir('./temp'):
+os.chdir(target_workdir)
+if not os.path.isdir('.hakuchumu'):
     print('対象ディレクトリ内に「.hakuchumu」が見つかりません')
-    exit(1)
+    os.mkdir('.hakuchumu')
+os.chdir('.hakuchumu')
+
+# テスト対象の設定フィルダに入っていて欲しいもの
+f = open('workdir_setting.txt', 'w')
+f.close()
+f = open('url_list.txt', 'w')
+f.close()
