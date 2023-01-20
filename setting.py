8c6dc296b779796fe9e84ce4ed2ba5249ccf37e4
AtsushiUtsumi <debtgx103@gmail.com>
2023-01-20 11:03:03 +0900

    「.hakuchumu」ディレクトリを返すように変更

--- setting.py
+++ setting.py
@@ -2,9 +2,12 @@
 class Settings:
     # コンストラクタにディレクトリを取って各変数を初回化するようにする
     url_list = [""]
+    hakuchum_dir = ''
     @staticmethod
     def get_urls():
         return Settings.url_list
+    def get_hakuchum_dir():
+        return Settings.hakuchum_dir
 
 def get_setting():
     settings = dict()
