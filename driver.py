98e410a23a71074e8280e72392e6743e3ead69de
AtsushiUtsumi <debtgx103@gmail.com>
2023-06-13 21:54:01 +0900

    chromedriverが無ければエラーにする

--- driver.py
+++ driver.py
@@ -6,16 +6,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome import service as fs
 from selenium.webdriver.chrome.options import Options
 
+
 def get_driver():
-    # TODO: 環境変数から取得するように変更する
-    CHROMEDRIVER = './chromedriver'
-    options = Options()
+    try:
+        # TODO: 環境変数から取得するように変更する
+        CHROMEDRIVER = './chromedriver'
+        options = Options()
+        # ヘッドレスで起動するオプションを指定
+        # options.headless = True
+        # 不要なエラーを非表示
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    # ヘッドレスで起動するオプションを指定
-    # options.headless = True
-    # 不要なエラーを非表示
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-    chrome_service = fs.Service(executable_path=CHROMEDRIVER)
-    driver = webdriver.Chrome(service=chrome_service, options=options)
-    return driver
+        chrome_service = fs.Service(executable_path=CHROMEDRIVER)
+        driver = webdriver.Chrome(service=chrome_service, options=options)
+        return driver
+    except:
+        print('ドライバエラー')
+        exit(0)
